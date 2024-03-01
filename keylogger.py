4559683c0d650f014321359104bffee5f88186eb
Thomas Arnold <ybn4aq@virginia.edu>
2024-03-01 12:36:51 -0500

    error checking

--- keylogger.py
+++ keylogger.py
@@ -10,11 +10,15 @@ if option == "1":
 elif option == "0":
     pass
 else:
-    print("Eror! Invalid input. Please type either 1 or 0. Exiting program now.")
+    print("Eror! Invalid input. Please type either 1 or 0. Exiting program.")
     exit(1)
 if domain:
     target = input("Type the domain name. ")
-    target_sock = socket.gethostbyname(target)  # TODO: error handling
+    try:
+        target_sock = socket.gethostbyname(target)  # TODO: error handling
+    except socket.gaierror:
+        print("Error! Invalid domain name or IP address. Exiting program.")
+        exit(1)
 else:
     target = input("Type an IP address to scan their ports. ")
     target_sock = target
@@ -34,9 +38,12 @@ print(
 socket.setdefaulttimeout(1)
 for i in range(start_port, end_port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    if sock.connect_ex((target_sock, i)) == 0:
-        print("Port " + str(i) + " is open.")
-    else:
-        print("Port " + str(i) + " is closed.")
-    sock.close()
+    try:
+        if sock.connect_ex((target_sock, i)) == 0:
+            print("Port " + str(i) + " is open.")
+        else:
+            print("Port " + str(i) + " is closed.")
+        sock.close()
+    except socket.error:
+        print("Error occured on port " + str(i))
 print("Port scanning complete.")
